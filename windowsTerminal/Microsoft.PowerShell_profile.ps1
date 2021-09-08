e41c3f34e1f5b6a5d26d9cc60eadfeb7683bb944
louiscavalcante <louiscavalcante@hotmail.com>
2021-09-08 19:41:26 -0400

    added GoAdmin function

--- windowsTerminal/Microsoft.PowerShell_profile.ps1
+++ windowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,5 @@ if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force }
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt anakmun
+function GoAdmin { Start-Process pwsh –Verb RunAs }
+Clear-Host
